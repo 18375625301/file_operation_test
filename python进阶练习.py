d315aa4f2980fef21b58bb53557482bd08cadfdc
hepengyi <522497177@qq.com>
2020-06-04 17:05:52 +0800

    None

---
+++ python进阶练习.py
@@ -0,0 +1,39 @@
+import  pickle
+
+
+# a1='贺鹏艺'
+# a2=234
+# a3=[10,20,30,40]
+#
+# with open('data.dat','wb') as f:
+#     pickle.dump(a1,f)
+#     pickle.dump(a2,f)
+#     pickle.dump(a3,f)
+#
+# with open('data.dat','rb') as f:
+#     b1=pickle.load(f)
+#     b2=pickle.load(f)
+#     b3=pickle.load(f)
+#     print(b1,b2,b3)
+
+
+###测试CSV文件
+# import csv
+#
+# with open('lianxi.csv','r') as f:
+#     a_csv=csv.reader(f)
+# #    print(list(a_csv))
+#     for row in a_csv:
+#         print(row)
+# with open('ee.csv','w') as f:
+#     b_csv=csv.writer(f)
+#     b_csv.writerow(['ID','姓名','年龄',])
+
+
+import os
+# os.system("notepad.exe")
+# os.system('ping www.baidu.com')
+#os.system('cmd')
+
+#直接调用可执行文件
+#os.startfile(r'')
